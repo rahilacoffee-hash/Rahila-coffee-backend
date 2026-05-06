865e6ea733b4ae39b7790675df9cb4f480b34df8
fred <abrahamfred123@gmail.com>
2026-05-06 22:35:33 +0100

    edit vite

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(
   cors({
     origin: function (origin, callback) {
       const allowedOrigins = [
-        "http://localhost:5174",
+        "http://localhost:5173",
         "http://localhost:3000",
         "https://rahila-coffeee.vercel.app", // your main domain
       ];
