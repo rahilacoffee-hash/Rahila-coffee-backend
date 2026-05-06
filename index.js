7a303ea860d587f3131e93454eca8c4749092990
fred <abrahamfred123@gmail.com>
2026-05-06 23:56:12 +0100

    edit vite

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(
       const allowedOrigins = [
         "http://localhost:5173",
         "http://localhost:3000",
-        "https://rahila-coffeeee.vercel.app", // your main domain
+        "https://rahilaa-coffee.vercel.app", // your main domain
       ];
 
       // Allow any Vercel preview deployment for your project
